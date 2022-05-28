47a14f2909ee445fb68dba4fd4552e0f5e3e1325
jeandtx <jean.doutriaux@gmail.com>
2022-05-28 20:17:52 +0200

    ready for upload

--- src/js/form.js
+++ src/js/form.js
@@ -10,32 +10,30 @@ function submitForm() {
 
 function add() {
     var formResult = {
-        task: form.elements["name"].value,
-        date: form.elements["email"].value,
-        category: form.elements["comment"].value,
+        name: form.elements["name"].value,
+        email: form.elements["email"].value,
+        comment: form.elements["comment"].value,
     }
     console.log(formResult);
 
-    // var tr = document.createElement("tr");
+    var tr = document.createElement("tr");
 
-    // var td1 = document.createElement("td");
-    // td1.textContent = formResult.task;
-    // var td12 = document.createElement("td");
-    // td12.textContent = "Details are not in the form";
-    // var td2 = document.createElement("td");
-    // td2.textContent = formResult.date;
-    // var td3 = document.createElement("td");
-    // td3.textContent = formResult.category;
+    var td1 = document.createElement("td");
+    td1.textContent = formResult.name;
+    var td2 = document.createElement("td");
+    td2.textContent = formResult.email;
+    var td3 = document.createElement("td");
+    td3.textContent = formResult.comment;
 
-    // tr.appendChild(td1);
-    // tr.appendChild(td12);
-    // tr.appendChild(td2);
-    // tr.appendChild(td3);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
 
-    // console.log(tr);
-    // if (formResult.task != "" && formResult.date != "" && formResult.category != "") {
-    //     document.getElementById("table").appendChild(tr);
-    // } else {
-    //     alert("Veuillez remplir tous les champs");
-    // }
+    console.log(tr);
+    if (formResult.name != "" && formResult.email != "" && formResult.comment != "") {
+        document.getElementById("table").appendChild(tr);
+        console.log("added");
+    } else {
+        alert("Veuillez remplir tous les champs");
+    }
 }
