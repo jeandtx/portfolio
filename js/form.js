844c7474d72736ee252f2a72ce0be48f931a9d3c
jeandtx <jean.doutriaux@gmail.com>
2022-05-29 18:37:45 +0200

    add comments and delete them

--- js/form.js
+++ js/form.js
@@ -1,39 +1,48 @@
-var form = document.forms.myForm;
+var form = document.forms.data;
 
 function submitForm() {
-    console.log(form);
-    add();
-    alert('Form submitted');
-}
-
-
-
-function add() {
-    var formResult = {
+    var result = {
         name: form.elements["name"].value,
         email: form.elements["email"].value,
         comment: form.elements["comment"].value,
     }
-    console.log(formResult);
 
-    var tr = document.createElement("tr");
-
-    var td1 = document.createElement("td");
-    td1.textContent = formResult.name;
-    var td2 = document.createElement("td");
-    td2.textContent = formResult.email;
-    var td3 = document.createElement("td");
-    td3.textContent = formResult.comment;
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-
-    console.log(tr);
-    if (formResult.name != "" && formResult.email != "" && formResult.comment != "") {
-        document.getElementById("table").appendChild(tr);
-        console.log("added");
-    } else {
-        alert("Veuillez remplir tous les champs");
-    }
+    let formResult = JSON.parse(localStorage.getItem("form"));
+    formResult = [...formResult, result];
+    localStorage.setItem("form", JSON.stringify(formResult));
 }
+
+
+function refresh() {
+    var formResult = JSON.parse(localStorage.getItem("form"));
+
+    formResult.forEach(element => {
+
+        var tr = document.createElement("tr");
+
+        var td1 = document.createElement("td");
+        td1.textContent = element.name;
+        var td2 = document.createElement("td");
+        td2.textContent = element.email;
+        var td3 = document.createElement("td");
+        td3.textContent = element.comment;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        if (element.name != "" && element.email != "" && element.comment != "") {
+            document.getElementById("table").appendChild(tr);
+        } else {
+            alert("Veuillez remplir tous les champs");
+        }
+    });
+
+}
+
+function clearTable() {
+    localStorage.clear();
+    localStorage.setItem("form", JSON.stringify([]));
+    location.reload();
+}
+
+refresh();
